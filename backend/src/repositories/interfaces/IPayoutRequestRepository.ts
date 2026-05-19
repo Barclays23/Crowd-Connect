61dbc6ba48afbde57ffa4cd990c85f4b3b070fbe
Barclays23 <itsmesajeer@gmail.com>
2026-05-19 22:00:19 +0530

    fixed build run bugs

--- backend/src/repositories/interfaces/IPayoutRequestRepository.ts
+++ backend/src/repositories/interfaces/IPayoutRequestRepository.ts
@@ -1,16 +1,16 @@
-// backend/src/repositories/interfaces/IPayoutRequestRepository.ts
+// // backend/src/repositories/interfaces/IPayoutRequestRepository.ts
 
-import { Types } from "mongoose";
-import { IPayoutRequestModel, GetPayoutRequestsFilter, PAYOUT_REQUEST_STATUS } from "@/types/wallet.types";
-
-
+// import { Types } from "mongoose";
+// import { IPayoutRequestModel, GetPayoutRequestsFilter, PAYOUT_REQUEST_STATUS } from "@/types/wallet.types";
 
 
-export interface IPayoutRequestRepository {
-  createPayoutRequest(data: Omit<IPayoutRequestModel, "_id" | "createdAt" | "updatedAt">): Promise<IPayoutRequestModel>;
-  findById(id: string): Promise<IPayoutRequestModel | null>;
-  findByEventId(eventId: string | Types.ObjectId): Promise<IPayoutRequestModel | null>;   // prevent duplicate payout requests
-  findAll(filters: GetPayoutRequestsFilter): Promise<{ payoutRequests: IPayoutRequestModel[]; total: number }>;
-  findByHostId(hostId: string | Types.ObjectId, filters: GetPayoutRequestsFilter): Promise<{ payoutRequests: IPayoutRequestModel[]; total: number }>;
-  updateStatus(id: string, status: PAYOUT_REQUEST_STATUS, reviewedBy?: string | Types.ObjectId, extras?: Partial<IPayoutRequestModel>): Promise<IPayoutRequestModel | null>;
-}
+
+
+// export interface IPayoutRequestRepository {
+//   createPayoutRequest(data: Omit<IPayoutRequestModel, "_id" | "createdAt" | "updatedAt">): Promise<IPayoutRequestModel>;
+//   findById(id: string): Promise<IPayoutRequestModel | null>;
+//   findByEventId(eventId: string | Types.ObjectId): Promise<IPayoutRequestModel | null>;   // prevent duplicate payout requests
+//   findAll(filters: GetPayoutRequestsFilter): Promise<{ payoutRequests: IPayoutRequestModel[]; total: number }>;
+//   findByHostId(hostId: string | Types.ObjectId, filters: GetPayoutRequestsFilter): Promise<{ payoutRequests: IPayoutRequestModel[]; total: number }>;
+//   updateStatus(id: string, status: PAYOUT_REQUEST_STATUS, reviewedBy?: string | Types.ObjectId, extras?: Partial<IPayoutRequestModel>): Promise<IPayoutRequestModel | null>;
+// }
