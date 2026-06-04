2e47f3642a0ca7bee1306b99ddea7920ff6718b7
Barclays23 <itsmesajeer@gmail.com>
2026-06-04 21:01:17 +0530

    fix(backend): fixed nodemailer configuration. explicitly provided port 465, host, secure: true

--- backend/src/utils/email.utils.ts
+++ backend/src/utils/email.utils.ts
@@ -10,27 +10,33 @@ interface SendEmailOptions {
 export async function sendEmail({ toAddress, mailSubject, text, htmlTemplate }: SendEmailOptions) {
   try {
     const transporter = nodemailer.createTransport({
-      service: process.env.SMTP_SERVICE,
-      auth: {
+      // service   : process.env.SMTP_SERVICE,  
+      // service is replaced by the host, port, secure  (below)
+      // by explicitly declaring port: 465 and secure: true, you are forcing Nodemailer to use standard, encrypted HTTPS-style traffic
+      host      : "smtp.gmail.com",
+      port      : 465,               
+      secure    : true,
+      auth      : {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS
       }
     });
 
     const mailOptions = {
-      from: process.env.SMTP_USER,
-      to: toAddress,
-      subject: mailSubject || "No Subject",
+      from    : process.env.SMTP_USER,
+      to      : toAddress,
+      subject : mailSubject || "No Subject",
       text,
-      html: htmlTemplate
+      html    : htmlTemplate
     };
 
     const info = await transporter.sendMail(mailOptions);
     console.log("Email sent successfully", );
 
   } catch (error: unknown) {
-    console.error("Email sending failed:", error);
-    throw new Error(`Could not send email: ${error}`);
+    console.error("🚨 Nodemailer Error Details:", error); 
+    const errorMessage = error instanceof Error ? error.message : String(error);
+    throw new Error(`Could not send email: ${errorMessage}`);
   }
 }
 
